4974df09b9764fc35835c696f26b174e622d5080
Isaac Silva <isaackun98@gmail.com>
2022-03-07 11:31:06 -0300

    Added a class called quizBrain

--- quizzler_01ver/lib/main.dart
+++ quizzler_01ver/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -28,6 +31,7 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
+/*
   List<String> questions = [
     'You can lead a cow down stairs but not up stairs.',
     'Approximately one quarter of human bones are in the feet.',
@@ -36,10 +40,9 @@ class _QuizPageState extends State<QuizPage> {
 
   List<bool> answers = [false, true, true];
 
-  int questionNumber = 0;
+  */
 
-  Question q1 =
-      Question('You can lead a cow down stairs but not up stairs.', false);
+  int questionNumber = 0;
 
   void changeQuestion() {
     questionNumber = questionNumber + 1;
@@ -57,7 +60,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quizBrain.getQuestionText(questionNumber),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -83,6 +86,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  bool correctAnswer =
+                      quizBrain.getQuestionAnswer(questionNumber);
+
+                  if (correctAnswer == true) {
+                    print('user got it right!');
+                  } else {
+                    print('user got it wrong');
+                  }
+
                   //metodo 1
                   changeQuestion();
                 });
@@ -107,6 +119,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  bool correctAnswer2 =
+                      quizBrain.getQuestionAnswer(questionNumber);
+
+                  if (correctAnswer2 == false) {
+                    print('user got it right');
+                  } else {
+                    print('user got it wrong');
+                  }
                   //metodo 2
                   questionNumber++;
                 });
